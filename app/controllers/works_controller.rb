7e6ca9292b674dcdc649c7c70c81fd70a5c1ca66
Matias Oñate <mationate.0@gmail.com>
2019-02-13 16:49:01 -0300

    work system with person and project

---
+++ app/controllers/works_controller.rb
@@ -0,0 +1,35 @@
+class WorksController < ApplicationController
+  before_action :set_proyect
+  before_action :set_person
+  def new
+    @work = Work.new
+  end
+  
+
+  def create
+    work = Work.new(work_params)
+    work.proyect = @proyect
+    work.person = @person
+    if work.save
+      redirect_to @proyect, notice: 'Se ha añadido un trabajador al proyecto'
+    else
+      redirect_to new_proyect_person_work_path(@proyect, @person), alert:'Hubo un error, intente nuevamente.'
+    end
+  end
+
+  private
+
+  def set_proyect
+    @proyect = Proyect.find(params[:proyect_id])
+  end
+
+  def set_person
+    @person = Person.find(params[:person_id])
+  end
+
+  def work_params
+    params.require(:work).permit(:fee, :start_date, :finish_date)
+  end
+  
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  resources :proyects
+  resources :proyects do
+    resources :people, only: [] do
+      resources :works, only: %i[create new]
+    end
+  end
   resources :people
   devise_for :admins
+
+
 
   root 'proyects#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
