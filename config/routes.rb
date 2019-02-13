7e2d0f2e035ee4a5251a1739e5f57528c16a2b49
Matias Oñate <mationate.0@gmail.com>
2019-02-13 15:23:40 -0300

    navbar bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :proyects
   resources :people
   devise_for :admins
+
+  root 'proyects#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
